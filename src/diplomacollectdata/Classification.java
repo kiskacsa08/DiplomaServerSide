edcb533535efb0839ab000feab073bfeceb9300e
Attila Horváth <ahorvath1991@gmail.com>
2016-01-09 23:13:49 +0100

    Clean implemented

--- src/diplomacollectdata/Classification.java
+++ src/diplomacollectdata/Classification.java
@@ -1,10 +1,26 @@
 package diplomacollectdata;
 
+
+import java.io.IOException;
+import org.rosuda.JRI.*;
+
 /**
  *
  * @author Attila
  */
 public class Classification {
+    
+    public static void Clean() {
+        Rengine re=new Rengine (new String [] {"--vanilla"}, false, null);
+        if (!re.waitForR())
+        {
+          System.out.println ("Cannot load R");
+          return;
+        }
+        re.eval("source('datacleaner.r')");
+        re.end();
+    }
+    
     public static void Classificate(){
         
     }
